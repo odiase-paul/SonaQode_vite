a4aed25f3a6efb2b290fc67443cf57a05da86432
Paul Akhere <paulakhere48@gmail.com>
2025-04-22 13:15:59 +0100

    latest commit

--- src/component/ReuseableComponent/ServiceSecondDiv.jsx
+++ src/component/ReuseableComponent/ServiceSecondDiv.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ServiceSecondDiv = ({ heading, arrays, button }) => {
   return (
@@ -37,11 +38,11 @@ const ServiceSecondDiv = ({ heading, arrays, button }) => {
               </div>
 
               <div className="sds-learn-more-div d-flex justify-content-center">
-                <a className="" href={item.buttonLink}>
+                <Link className="" to={item.buttonLink}>
                   <button className="btn rounded-pill border-0  sds-learn-more">
                     {button}
                   </button>
-                </a>
+                </Link>
               </div>
             </div>
           );
